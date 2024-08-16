30dd888b1df15a556b2017ccf47baedb1ba5f142
Simon - fantomitechno <simon@renoux.dev>
2024-08-16 22:21:59 +0200

    🐛 ms

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -5,7 +5,7 @@ export const toTime = (text: string) => {
 	time += Number(ms);
 
 	const rawTimes = rawTime.split(':').map(Number).reverse();
-	const defaltMultiplier = 100;
+	const defaltMultiplier = 1000;
 
 	for (let i = 0; i < rawTimes.length; i++) {
 		time += rawTimes[i] * defaltMultiplier * Math.pow(60, i);
@@ -17,20 +17,20 @@ export const toTime = (text: string) => {
 export const fromTime = (time: number | null) => {
 	if (time == null) return;
 	let text = '';
-	const hours = Math.floor(time / (60 * 60 * 100));
-	time = time % (60 * 60 * 100);
+	const hours = Math.floor(time / (60 * 60 * 1000));
+	time = time % (60 * 60 * 1000);
 
-	const minutes = Math.floor(time / (60 * 100));
+	const minutes = Math.floor(time / (60 * 1000));
 	let minutesTime = '';
 	if (minutes < 10 && hours) minutesTime += '0';
 	minutesTime += `${minutes}`;
-	time = time % (60 * 100);
+	time = time % (60 * 1000);
 
-	const seconds = Math.floor(time / 100);
+	const seconds = Math.floor(time / 1000);
 	let secondsTime = '';
 	if (seconds < 10) secondsTime += '0';
 	secondsTime += `${seconds}`;
-	time = time % 100;
+	time = time % 1000;
 
 	const ms = time;
 	let msTime = '';
